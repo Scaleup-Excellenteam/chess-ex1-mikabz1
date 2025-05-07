51ac1ac0493d490f4f28f92a90ffa1f742a8aa4b
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-05-07 14:53:53 +0300

    Update Pieces.cpp

--- src/Pieces.cpp
+++ src/Pieces.cpp
@@ -7,12 +7,7 @@
 #include "Pawn.h"
 #include "Bishop.h"
 #include "MyExceptions.h"
-/**
- * @brief Constructs a Pieces object from a string representing the chessboard.
- * Initializes the chessboard by interpreting a string of piece representations.
- * The string must have a length of BOARD_CELL_NUMBER.
- * @param str A string representing the initial state of the chessboard.
- */
+
 Pieces::Pieces(const std::string &str) {
     if (str.size() != BOARD_CELL_NUMBER) {
         std::cerr << "Invalid board string length: expected " << BOARD_CELL_NUMBER
@@ -44,46 +39,29 @@ Pieces::Pieces(const std::string &str) {
         }
     }
 }
-/**
- * @brief Copy constructor for creating a deep copy of another Pieces object.
- *
- * @param board The Pieces object to copy.
- */Pieces::Pieces(const Pieces &board) {
+Pieces::Pieces(const Pieces &board) {
     for(const auto& element : board._board){
         _board[element.first] = element.second->clone();
     }
 }
-/**
- * @brief Checks if a specific box on the board is occupied by a piece.
- * @param location The coordinate of the box to check.
- * @return True if the box is occupied, otherwise false.
+
  */bool Pieces::isOccupiedBox(const Coordinate& location) const {
     return _board.find(location) != _board.end();
 }
-/**
- * @brief Adds a piece to the board at its current location.
- * @param piece The piece to add to the board.
- */
+
 void Pieces::addPiece(const std::shared_ptr<Piece>& piece){
     if(piece == nullptr){
         throw NullPiece("FATAL ERROR: cannot add null piece to the board");
     }
     _board[piece->getLocation()] = piece;
 }
-/**
- * @brief Removes a piece from the board at the specified location.
- * @param location The coordinate of the piece to remove.
- */
+
 void Pieces::removePiece(const Coordinate& location) {
     if(isOccupiedBox(location)){
         _board.erase(location);
     }
 }
-/**
- * @brief Gets the location of the king of a specific color.
- * @param color The color of the king to find (either WHITE or BLACK).
- * @return The coordinate of the king's location.
- */
+
 Coordinate Pieces::getKingLocation(COLOR color) const {
     for(auto &piece : _board){
         if(piece.second->getId() == KING && piece.second->getColor() == color){
@@ -92,11 +70,7 @@ Coordinate Pieces::getKingLocation(COLOR color) const {
     }
     return {ROW_PLACE_HOLDER , COLL_PLACE_HOLDER};
 }
-/**
- * @brief Retrieves a piece at a specific location on the board.
- * @param location The coordinate of the piece to retrieve.
- * @return A reference to the piece at the given location.
- */
+
 const std::shared_ptr<Piece>& Pieces::getPiece(const Coordinate& location)const{
     static const std::shared_ptr<Piece> nullPiece = nullptr;
     if(_board.find(location) == _board.end()){
@@ -104,11 +78,7 @@ const std::shared_ptr<Piece>& Pieces::getPiece(const Coordinate& location)const{
     }
     return _board.at(location);
 }
-/**
- * @brief Gets all potential moves for all pieces of a specific color.
- * @param color The color of the pieces to get moves for.
- * @return A set of coordinates representing the potential moves of all pieces of the specified color.
- */
+
 std::set<Coordinate> Pieces::getPiecesPotenMoves(COLOR color) {
     std::set<Coordinate> result;
     for(auto &element : _board){
@@ -119,11 +89,7 @@ std::set<Coordinate> Pieces::getPiecesPotenMoves(COLOR color) {
     }
     return result;
 }
-/**
- * @brief Gets all raw moves for all pieces of a specific color.
- * @param color The color of the pieces to get raw moves for.
- * @return A set of coordinates representing the raw moves of all pieces of the specified color.
- */
+
 std::set<Coordinate> Pieces::getPiecesRawMoves(COLOR color) {
     std::set<Coordinate> result;
     for(auto &element : _board){
@@ -134,34 +100,20 @@ std::set<Coordinate> Pieces::getPiecesRawMoves(COLOR color) {
     }
     return result;
 }
-/**
- * @brief Checks if the king of a specific color is in check.
- * @param color The color of the king to check.
- * @return True if the king is in check, otherwise false.
- */
+
 bool Pieces::isCheck(COLOR color) {
     return getPiecesPotenMoves(colorNot(color)).contains(getKingLocation(color));
 
 }
-/**
- * @brief Moves a piece to a new location on the board.
- * Updates the piece's location and removes the piece from its old location.
- * @param destination The new location to move the piece to.
- * @param piece The piece to move.
- */
+
 void Pieces::pieceMove(const Coordinate& destination,const std::shared_ptr<Piece>& piece){
     Coordinate currentLocation = piece->getLocation();
     piece->setLocation(destination);
     piece->setHasMoved(true);
     addPiece(piece);
-    //here will be pawn logic.
     removePiece(currentLocation);
 }
-/**
- * @brief Updates the potential moves for all pieces of a specific color.
- * Iterates over all pieces, updating their potential moves based on the current board state.
- * @param color The color of the pieces to update.
- */
+
 void Pieces::updatePotenMoves(COLOR color) {
     for(auto &element : _board){
         element.second->setIsProtected(false);
@@ -178,14 +130,11 @@ void Pieces::updatePotenMoves(COLOR color) {
     }
 }
 
-/// @brief Retrieves all legal moves for the specified color.
-/// @param color The color of the player whose legal moves are to be retrieved.
-/// @return A vector of `Move` objects representing all the legal moves for the given color.
+
 std::vector<Move> Pieces::getAllLegalMoves(COLOR color)const{
     std::vector<Move> result;
-    // Iterate through each piece on the board
+    
     for(const auto& [coord, piece] : _board){
-        // Skip pieces that do not match the specified color
         if(piece->getColor() != color)continue;
         // Retrieve potential moves for the piece
         for(const auto& destination : piece->getPotenMoves()){
@@ -195,31 +144,23 @@ std::vector<Move> Pieces::getAllLegalMoves(COLOR color)const{
     return result;
 }
 
-/// @brief Makes a move for a piece to the specified destination. If the piece is a pawn and can promote, handles the promotion process.
-/// @param destination The coordinate to which the piece should move.
-/// @param piece A shared pointer to the piece being moved.
 void Pieces::makeMove(const Coordinate & destination,const std::shared_ptr<Piece>& piece) {
     auto pieceToMove = piece;
-    // If the piece is a pawn, check for promotion
     if (piece->getId() == PAWN){
         auto pawn = std::dynamic_pointer_cast<Pawn>(piece);
-        // Check if the pawn can promote
         if (pawn->canPromote() && pawn){
             try {
-                // Let the user choose the promotion type
                 pawn->userPromotionChoice(pawn->getLocation());
-                pieceToMove = pawn->getPromotedPiece();// Get the newly promoted piece
-                addPiece(pieceToMove);// Add the promoted piece to the board
+                pieceToMove = pawn->getPromotedPiece();
+                addPiece(pieceToMove);
             }
             catch (WrongPromotionInput& e){
-                // Handle invalid promotion input
                 std::cout << e.what();
             }
 
 
         }
     }
-    // Move the piece to the destination
     pieceMove(destination,pieceToMove);
 }
 
