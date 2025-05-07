4ec25a4eb8c7aec8796890e4e45e0f58dc1e5eed
Michael Benzekri <110330086+mikabz1@users.noreply.github.com>
2025-05-07 14:51:39 +0300

    Update Pawn.cpp

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -7,20 +7,8 @@
 #include "Constants.h"
 #include "MyExceptions.h"
 
-/**
- * @brief Constructs a Pawn object.
- * Initializes a Pawn with the specified color and location on the chessboard.
- * @param color The color of the Pawn (either WHITE or BLACK).
- * @param location The initial location of the Pawn on the board.
- */
 Pawn::Pawn(COLOR color,const Coordinate& location): Piece(PAWN , color , location),promotedPiece(nullptr) {}
-/**
- * @brief Returns all valid moves for the Pawn.
- * Calculates the Pawn's potential moves based on its current position and whether it has moved before. The function
- * checks for possible forward moves and diagonal captures.
- * @param board The current chessboard state.
- * @return A vector of coordinates representing the valid moves for the Pawn.
- */
+
 std::vector<Coordinate> Pawn::getRawMoves(const Pieces &board) {
     std::vector<Coordinate> result;
 
@@ -57,14 +45,12 @@ std::vector<Coordinate> Pawn::getRawMoves(const Pieces &board) {
     return result;
 }
 
-/// @brief Retrieves the promoted piece of the pawn, if it has been promoted.
-/// @return A constant reference to the shared pointer of the promoted piece.
+
 const std::shared_ptr<Piece> &Pawn::getPromotedPiece() const {
     return promotedPiece;
 }
 
-/// @brief Handles user input for promoting the pawn to another piece (Queen, Rook, Bishop, or Knight).
-/// @param promotionLocation The coordinate where the promotion is taking place.
+
 void Pawn::userPromotionChoice(const Coordinate & promotionLocation) {
     bool correctInput = false;
     std::string choice;
@@ -102,24 +88,16 @@ void Pawn::userPromotionChoice(const Coordinate & promotionLocation) {
     }
 
 }
-/// @brief Checks if the pawn is eligible for promotion based on its current location and color.
-/// @return True if the pawn can be promoted (i.e., it has reached the last row for white or first row for black).
+
 bool Pawn::canPromote() {
     if (getColor() == WHITE){
-        // White pawn can promote when it reaches the last row
         return  static_cast<char>(getLocation().first + 1) == LAST_ROW;
     }
     else{
-        // Black pawn can promote when it reaches the first row
         return static_cast<char>(getLocation().first - 1) == FIRST_ROW;
        }
 }
 
-/**
- * @brief Creates a clone of the Pawn.
- * Returns a new shared pointer to a copy of the current Pawn.
- * @return A shared pointer to the cloned Pawn.
- */
 std::shared_ptr<Piece> Pawn::clone() const {
     return std::make_shared<Pawn>(*this);
 }
